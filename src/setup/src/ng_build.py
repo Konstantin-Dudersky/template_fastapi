5d5d21c500109140ee1990e1e060357dd824bc1b
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2022-08-09 12:07:05 +0300

    update

--- src/setup/src/ng_build.py
+++ src/setup/src/ng_build.py
@@ -5,6 +5,7 @@ from typing import Callable
 def main(
     work_dir_relative: str,
     project: str = "client",
+    base_href: str = "/",
 ) -> Callable[[], None]:
     def _main():
         curr_dir = os.getcwd()
@@ -12,7 +13,7 @@ def main(
         work_dir_abs = os.path.abspath(work_dir_abs_full)
         print(f"-> Рабочая папка: {work_dir_abs}")
         os.chdir(work_dir_abs)
-        os.system("ng build")
+        os.system(f"ng build --base-href {base_href}")
         filename = f"dist/{project}.zip"
         os.system(f"zip -j -r {filename} dist/{project}/")
         print(f"-> Создан архив: {filename}")
